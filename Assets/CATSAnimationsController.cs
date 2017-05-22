0f2bbe4b91191c3c445616498f5acb2d712d2a8f
Yakov Scherba <sherbayv@gmail.com>
2017-05-22 09:23:02 +0600

    image animation

--- Assets/CATSAnimationsController.cs
+++ Assets/CATSAnimationsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class CATSAnimationsController : MonoBehaviour {
 
@@ -33,29 +34,50 @@ public class CATSAnimationsController : MonoBehaviour {
 	}
 
 	private void ITweenAnimate() {
-		_detailName.transform.localScale = _detailImage.transform.localScale = _detailParams.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-		float rotationPeriod = BASE_ANIMATION_DURATION / 8f;
-		Hashtable rotationParams = iTween.Hash (
-			"z"      	, 90f,
-			"time"    	, rotationPeriod,
-			"looptype"  , iTween.LoopType.loop
-		);
+        Color newColor = _detailImage.transform.GetComponent<Image>().color;
+        newColor.a = 0.7f;
+        _detailImage.transform.GetComponent<Image>().color = newColor;
+        _detailName.transform.localScale = _detailImage.transform.localScale = _detailParams.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        float rotationPeriod = BASE_ANIMATION_DURATION * 0.8f;// BASE_ANIMATION_DURATION / 10f;
+        float rotationTimes = -20f;
+        Hashtable rotationParams = iTween.Hash(
+            "z"      	, 360f * rotationTimes,
+            "time", rotationPeriod
+        );
 
-		const iTween.EaseType ITWEEN_EASE_TYPE = iTween.EaseType.easeInOutBack;
+		const iTween.EaseType ITWEEN_SCALE_EASE_TYPE = iTween.EaseType.easeInOutBack;
 		float scaleTime = BASE_ANIMATION_DURATION * 1.2f;
 		Hashtable scaleParams = iTween.Hash (
 			"scale"			  , Vector3.one,
 			"time"    		  , scaleTime,
-			"easeType"		  , ITWEEN_EASE_TYPE,
+			"easeType"		  , ITWEEN_SCALE_EASE_TYPE,
 			"oncomplete"	  , "OnITweenComplete",
 			"oncompletetarget", gameObject
 		);
 
-		iTween.ScaleTo (_detailImage, scaleParams);
+        const iTween.EaseType ITWEEN_ALPHA_EASE_TYPE = iTween.EaseType.easeInOutBack;
+        float unfadeTime = BASE_ANIMATION_DURATION;
+        Hashtable fadeParams = iTween.Hash(
+            "from", _detailImage.transform.GetComponent<Image>().color.a,
+            "to", 1f,
+            "name", "fadetween",
+            "time", unfadeTime,
+            "easetype", ITWEEN_ALPHA_EASE_TYPE,
+            "onupdate", "UpdateAlpha"
+        );
+
+        iTween.ValueTo(gameObject, fadeParams);
+        iTween.ScaleTo (_detailImage, scaleParams);
 		iTween.RotateTo (_detailImage, rotationParams);
 	}
 
-	private void OnITweenComplete() {
+    public void UpdateAlpha(float newValue) {
+        Color newColor = _detailImage.GetComponent<Image>().color;
+        newColor.a = newValue;
+        _detailImage.GetComponent<Image>().color = newColor;
+    }
+
+    private void OnITweenComplete() {
 		_detailImage.transform.localRotation = Quaternion.Euler (Vector3.zero);
 		iTween.Stop ();
 	}
